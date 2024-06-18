fbe48116c00b5ed6d4a47cff8376380f5694f89a
Babatunde Oladejo <tundeoladejo@outlook.com>
2024-06-19 00:23:46 +0100

    updated resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,7 +1,320 @@
+"use client";
+
+import { Description } from "@radix-ui/react-dialog";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaVuejs,
+  FaFigma,
+  FaNodeJs,
+  FaPhp,
+} from "react-icons/fa";
+
+import { HiMiniAcademicCap, HiBriefcase } from "react-icons/hi2";
+
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
+// about data
+const about = {
+  title: "About me",
+  description:
+    "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicos directe al desirabilite de un nov lingua franca: On refusa continuar payar custosi traductores. At solmen va esser necessi far uniform grammatica, pronunciation e plu sommun paroles. Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues. It va esser tam simplic quam Occidental in fact, it va esser Occidental.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Babatunde Oladejo",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+2348102992169)",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "5+ Years",
+    },
+    {
+      fieldName: "Skype",
+      fieldValue: "",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Nigerian",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "babatundeoladejo16@gmail.com",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, German",
+    },
+  ],
+};
+
+//experience data
+const experience = {
+  icon: <HiBriefcase />,
+  title: "My Experience",
+  description:
+    "Angleso it va semblar un simplificat Angles, quam un skeptic Cambridge amico dit me que Occidental es.Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.",
+  items: [
+    {
+      company: "Dufuna Technologies",
+      position: "Software Engineer (Frontend)",
+      duration: "2019 - Present",
+    },
+    {
+      company: "Cotta & Cush Ltd.",
+      position: "Software Engineer",
+      duration: "2018 - 2019",
+    },
+  ],
+};
+
+//education data
+const education = {
+  icon: <HiMiniAcademicCap />,
+  title: "My Education",
+  description: "Omnicos directe al desirabilite de un nov lingua franca",
+  items: [
+    {
+      institution: "Udacity",
+      degree: "Web Architecture",
+      duration: "2023",
+    },
+    {
+      institution: "udacity",
+      degree: "Mobile Web Specialist Nano degree",
+      duration: "2023",
+    },
+    {
+      institution: "Web Architecture",
+      degree: "Udacity",
+      duration: "2023",
+    },
+  ],
+};
+
+const skills = {
+  title: "My Skills",
+  description:
+    "But I must explain to you how all this mistaken idea of denouncing pleasure and praising",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "HTML",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "CSS 3",
+    },
+    {
+      icon: <FaJs />,
+      name: "Javascript",
+    },
+    {
+      icon: <FaReact />,
+      name: "React",
+    },
+    {
+      icon: <FaVuejs />,
+      name: "VueJs",
+    },
+    {
+      icon: <FaFigma />,
+      name: "Figma",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "Node",
+    },
+    {
+      icon: <FaPhp />,
+      name: "PHP",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "Tailwind",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "NextJs",
+    },
+  ],
+};
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { animate, motion } from "framer-motion";
+
 const Resume = () => {
   return (
-    <div>Resume page</div>
-  )
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: {
+          delay: 2.4,
+          duration: 0.4,
+          ease: "easeIn",
+        },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About Me</TabsTrigger>
+          </TabsList>
+
+          {/* content */}
+          <div className="min-h-[70vh] w-full">
+            {/* experience */}
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea>
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left ">
+                            {item.position}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            {/* dot */}
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            {/* education */}
+            <TabsContent value="education" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {education.description}
+                </p>
+                <ScrollArea>
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">
+                            {item.institution}
+                          </span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left ">
+                            {item.degree}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            {/* dot */}
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.duration}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            {/* skills */}
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{skills.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {skills.description}
+                </p>
+                <ScrollArea>
+                  <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px]">
+                    {skills.skillList.map((skill, index) => {
+                      return (
+                        <li key={index}>
+                          <TooltipProvider>
+                            <Tooltip>
+                              <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                                <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                                  {skill.icon}
+                                </div>
+                              </TooltipTrigger>
+                              <TooltipContent>
+                                <p className="capitalize">{skill.name}</p>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            {/* about */}
+            <TabsContent value="about" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+                <ScrollArea>
+                  <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[640px] mx-auto xl:mx-0">
+                    {about.info.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="flex items-center justify-center xl:justify-start gap-4 "
+                        >
+                          <span className="text-white/60">
+                            {item.fieldName}
+                          </span>
+                          <span className="text-xl ">{item.fieldValue}</span>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
